70b9c0c4348ccff9a37a52c2d3528d739ce308b6
LatifAsgarov <macbook@MacBooks-MacBook-Air-10.local>
2024-02-04 21:05:54 +0400

    add first demo

---
+++ Web2_fistApp/src/main/java/ada/edu/az/Web2_fistApp/Web2FistAppApplication.java
@@ -0,0 +1,13 @@
+package ada.edu.az.Web2_fistApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web2FistAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web2FistAppApplication.class, args);
+	}
+
+}
---
+++ Web2_fistApp/src/test/java/ada/edu/az/Web2_fistApp/Web2FistAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ada.edu.az.Web2_fistApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web2FistAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
